66eae15ca08486c41e9b2e9553d9fe80088bb486
ismailoxs <139073609+ismailoxs@users.noreply.github.com>
2024-04-15 19:37:58 +0100

    login reecreation and create account creation

--- Inter_Pro/app/src/main/java/com/example/inter_pro/MainActivity.java
+++ Inter_Pro/app/src/main/java/com/example/inter_pro/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,7 +21,10 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     Button submit,creationaccout;
     EditText user,code;
-    ImageView imageView;
+    TextView login,textView3,textView4,textView39;
+    LinearLayout linearLayout2;
+    ImageView imageView,ImageView11,imageView12;
+    View view5,view6;
         private AnimationDrawable animation;
 //    Intent intent;
     @Override
@@ -31,8 +36,15 @@ public class MainActivity extends AppCompatActivity {
          imageView= (ImageView) findViewById(R.id.companyicon);
         imageView.setBackgroundResource(R.drawable.animation);
         animation = (AnimationDrawable) imageView.getBackground();
+        login=findViewById(R.id.loginID);
         user=findViewById(R.id.userID);
-        code=findViewById(R.id.codeID);
+        textView3=findViewById(R.id.textView3);
+        textView4=findViewById(R.id.textView4);
+        textView39=findViewById(R.id.textView39);
+        linearLayout2=findViewById(R.id.linearLayout2);
+        ImageView11=findViewById(R.id.imageView11);
+        imageView12=findViewById(R.id.imageView12);
+
         creationaccout=findViewById(R.id.buttoncreationaccount);
 
         submit.setOnClickListener(new View.OnClickListener() {
@@ -64,6 +76,12 @@ public class MainActivity extends AppCompatActivity {
                 submit.setVisibility(View.VISIBLE);
                 code.setVisibility(View.VISIBLE);
                 creationaccout.setVisibility(View.VISIBLE);
+                imageView12.setVisibility(View.VISIBLE);
+                ImageView11.setVisibility(View.VISIBLE);
+                linearLayout2.setVisibility(View.VISIBLE);
+                textView39.setVisibility(View.VISIBLE);
+                textView4.setVisibility(View.VISIBLE);
+                textView3.setVisibility(View.VISIBLE);
             }
         }, 3000);  // 3000 milliseconds = 3 seconds
     }
